ca959f2fbc9f9988dedfb770b63f214308832d37
Ken <saganawski@gmail.com>
2021-05-10 17:42:30 -0400

    refoctor code using what we have learned from classes

--- objects_and_constructors/BookProject.js
+++ objects_and_constructors/BookProject.js
@@ -1,100 +1,114 @@
+//TODO: add a display class for manipulating the dom
+class Library {
+    books = [];
 
-let myLibrary = [];
+    get getBooks(){
+        return this.books;
+    }
 
-function Book(title,author,pages,isRead) {
-    this.author = author
-    this.title = title
-    this.pages = pages
-    this.isRead = isRead
+    addBookToLibrary(Book){
+        this.books.push(Book);
+    }
+
+    printBooks(){
+        console.log(this.books);
+    }
+
+    addBooksToTable() {
+        const books = document.querySelectorAll(".books");
+        this.removeElements(books);
+    
+        const buttons = document.querySelectorAll('button');
+        this.removeElements(buttons);
+    
+        let index = 0;
+        this.books.forEach(book => {
+            // append new rows (books) to table
+            var tableRow = document.getElementById("table");
+    
+            var newRow = `<tr class="books"> <td> ${book.title} </td> <td> ${book.author}</td> <td> ${book.pages} </td><td> ${book.isRead} </td> </tr>`;
+            tableRow.innerHTML += newRow;
+            
+            const button = document.createElement("button");
+            button.innerHTML= "Remove";
+            button.id = index;
+    
+            tableRow.append(button);
+    
+    
+            index++;
+        });
+    
+        this.addListenerToButtons();
+    }
+
+    removeElements(elms){
+        elms.forEach(el => el.remove());
+    }
+
+    addListenerToButtons() {
+        const removeButtons = document.querySelectorAll('button');
+
+        removeButtons.forEach(btn => {
+            btn.addEventListener('click', e => {
+                e.preventDefault();
+                const indexPosition = e.target.id;
+                this.books.splice(indexPosition, 1);
+
+                const button = document.getElementById(indexPosition)
+                if(button){
+                    button.remove();
+                }
+
+                this.addBooksToTable();
+            });
+        });
+    }
 
 }
 
-function addBookToLibrary(Book){
-    myLibrary.push(Book);
-}
+class Book {
+    constructor(title,author,pages,isRead){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+    };
 
-var dune = new Book("dune", "hubert", 1000, false);
-var sapiens = new Book("sapiens", "yuval", 1000, true);
-
-addBookToLibrary(dune);
-addBookToLibrary(sapiens);
-
-console.log(myLibrary);
-
-addBooksToTable();
-function addBooksToTable() {
-    // remove all elements in table first
-    const removeElements = (elms) => elms.forEach(el => el.remove());
-    const books = document.querySelectorAll(".books");
-    removeElements(books);
-
-    const buttons = document.querySelectorAll('button');
-    removeElements(buttons);
-
-    let index = 0;
-    myLibrary.forEach(book => {
-        // append new rows (books) to table
-        var tableRow = document.getElementById("table");
-
-        var newRow = "<tr class='books'> <td>" + book.title + "</td> <td>" + book.author + "</td> <td>" + book.pages + "</td><td>" + book.isRead + "</td> </tr>";
-        tableRow.innerHTML += newRow;
-        
-        const button = document.createElement("button");
-        button.innerHTML= "Remove";
-        button.id = index;
-
-        tableRow.append(button);
-
-
-        index++;
-    });
-
-    addListenerToButtons();
 }
 
 
-var addBookForm = document.getElementById("addBook").addEventListener("click",displayAddBookForm);
+const dune = new Book("dune", "hubert", 1000, false);
+const sapiens = new Book("sapiens", "yuval", 1000, true);
+const library = new Library();
+
+library.addBookToLibrary(dune);
+library.addBookToLibrary(sapiens);
+library.printBooks();
+library.addBooksToTable();
+library.addListenerToButtons();
+
+
+const addBookForm = document.getElementById("addBook").addEventListener("click",displayAddBookForm);
 
 function displayAddBookForm(){
     document.getElementById("form").style.display = "inline";
 }
 
 
-var submitNewBook = document.getElementById("form").addEventListener("submit", function(e){
+const submitNewBook = document.getElementById("form").addEventListener("submit", function(e){
     e.preventDefault();
 
-    var formData = new FormData(document.querySelector('form'));
-    let title = formData.get("title");
-    let author = formData.get("author");
-    let pages = formData.get("pages");
-    let isRead = formData.get("isRead");
+    const formData = new FormData(document.querySelector('form'));
+    const title = formData.get("title");
+    const author = formData.get("author");
+    const pages = formData.get("pages");
+    const isRead = formData.get("isRead");
     
     const newBook = new Book(title,author,pages,isRead);
 
-    myLibrary.push(newBook);
-
-    addBooksToTable();
+    library.addBookToLibrary(newBook);
+    library.addBooksToTable();
 
     document.getElementById("form").style.display = "none";
 });
-
-
-
-addListenerToButtons();
-
-function addListenerToButtons() {
-    const removeButtons = document.querySelectorAll('button');
-
-    removeButtons.forEach(btn => {
-        btn.addEventListener('click', e => {
-            e.preventDefault();
-
-            const indexPosition = e.target.id;
-            myLibrary.splice(indexPosition, 1);
-            const button = document.getElementById(indexPosition).remove();
-
-            addBooksToTable();
-        });
-    });
-}
-
